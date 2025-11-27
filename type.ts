39968227338a000d763079d7b91dab830eca89b8
gaabe007 <gabrielwillianutri@gmail.com>
2025-11-26 21:46:18 -0300

    Add files via upload

---
+++ type.ts
@@ -0,0 +1,13 @@
+// Função de multiplicação com tipagem
+function multiplicar(a: number, b: number): number {
+  return a * b;
+}
+
+// Função de saudação com tipagem
+function saudacao(nome: string): string {
+  return "Olá " + nome;
+}
+
+// Testes
+console.log(multiplicar(5, 3)); // 15
+console.log(saudacao("Gabriel")); // Olá Gabriel
